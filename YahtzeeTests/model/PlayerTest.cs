bb1be8806a960f1ffb5d28361c6cca2d08006721
Niall Thurrat <nt222fc@student.lnu.se>
2020-11-26 16:03:29 +0100

    RED

--- YahtzeeTests/model/PlayerTest.cs
+++ YahtzeeTests/model/PlayerTest.cs
@@ -3,6 +3,7 @@ using Xunit;
 using Xunit.Sdk;
 using Moq;
 using Yahtzee.model;
+using System.Collections.Generic;
 
 namespace YahtzeeTests.model
 {
@@ -19,6 +20,25 @@ namespace YahtzeeTests.model
             var actual = player.GetDice();
             
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetDiceShouldReturnTwoDie()
+        {
+            var random = new Random();
+            var mockDice = new List<Die>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                var mockDie = new Die(random);
+                mockDice.Add(mockDie);
+            }
+
+            var sut = new Player(mockDice);
+
+            var actual = sut.GetDice().Count;
+            
+            Assert.Equal(2, actual);
         }
     }
 }
